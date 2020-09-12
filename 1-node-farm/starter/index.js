9dc48e267276b08ee8194d871120ee0db411d04d
Elango <elango@Elangos-MacBook-Pro.local>
2020-09-12 15:28:57 +0530

    8-reading_writing_files

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,3 +1,8 @@
 const fs = require("fs");
-const name = "Hello world";
-console.log(name);
+
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textIn);
+
+const textOut = `This is what we know about avacado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File Written");
