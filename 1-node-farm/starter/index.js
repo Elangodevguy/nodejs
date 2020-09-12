ef92b0b9384b271b0125ff65e45173afe4d6f316
Elango <elango@Elangos-MacBook-Pro.local>
2020-09-12 17:12:23 +0530

    11-creating_simple_web_server

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,7 @@
 const fs = require("fs");
+const http = require("http");
+///////////////////////////
+// FILES
 // Blocking-synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 // console.log(textIn);
@@ -8,13 +11,23 @@ const fs = require("fs");
 // console.log("File Written");
 
 // Non-blocking: async way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      fs.writeFile("./txt/final.txt", `${data2}.\n${data3}`, "utf-8", (err) => {
-        console.log("Your file has been written");
-      });
-    });
-  });
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       fs.writeFile("./txt/final.txt", `${data2}.\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written");
+//       });
+//     });
+//   });
+// });
+// console.log("Will Read File!!!!");
+
+///////////////////////////
+// SERVER
+const server = http.createServer((req, res) => {
+  res.end("Hello from the node server");
 });
-console.log("Will Read File!!!!");
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listemning to request on port: 8000");
+});
