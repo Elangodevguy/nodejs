469f58345ba92162519f79658a848d465f5319a8
Elango <elango@Elangos-MacBook-Pro.local>
2020-09-16 22:11:03 +0530

    15-filling_the_template

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -21,20 +21,55 @@ const http = require("http");
 //   });
 // });
 // console.log("Will Read File!!!!");
+const replaceTemplate = (template, element) => {
+  let output = template.replace(/{%NAME%}/g, element.productName);
+  output = output.replace(/{%IMAGE%}/g, element.image);
+  output = output.replace(/{%FROM%}/g, element.from);
+  output = output.replace(/{%NUTRIENTS%}/g, element.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, element.quantity);
+  output = output.replace(/{%PRICE%}/g, element.price);
+  output = output.replace(/{%DESCRIPTION%}/g, element.description);
+
+  if (!element.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 ///////////////////////////
 // SERVER
 const server = http.createServer((req, res) => {
   const pathName = req.url;
+  // Overview Page
   if (pathName === "/overview" || pathName === "/") {
-    res.end("This is overview page");
-  } else if (pathName === "/product") {
+    const cards = dataObj.map((el) => replaceTemplate(tempCard, el)).join("");
+    const cardsHtml = tempOverview.replace(/{%CARDS%}/g, cards);
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(cardsHtml);
+  }
+  // Product Page
+  else if (pathName === "/product") {
     res.end("This is Product page");
-  } else if (pathName === "/api") {
+  }
+  // API
+  else if (pathName === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
-  } else {
+  }
+  // Not found
+  else {
     res.writeHead(404, {
       "Content-type": "text/html",
       "my-own": "Elango",
