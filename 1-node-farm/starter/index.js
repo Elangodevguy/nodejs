088de3fca86bb1f1f1d519806c0bc793a66fb6d2
Elango <elango@Elangos-MacBook-Pro.local>
2020-09-16 22:14:20 +0530

    15-filling_the_template_1

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -29,6 +29,7 @@ const replaceTemplate = (template, element) => {
   output = output.replace(/{%QUANTITY%}/g, element.quantity);
   output = output.replace(/{%PRICE%}/g, element.price);
   output = output.replace(/{%DESCRIPTION%}/g, element.description);
+  output = output.replace(/{%ID%}/g, element.id);
 
   if (!element.organic)
     output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
