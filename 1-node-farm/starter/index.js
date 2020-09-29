c2bbcf049812577178281bcab24e77606511bc1f
Elango <elango@Elangos-MacBook-Pro.local>
2020-09-29 20:12:07 +0530

    added nodemon

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+
+const slugify = require("slugify");
+
 const replaceTemplate = require("./modules/replaceTemplate");
 ///////////////////////////
 // FILES
@@ -37,6 +40,8 @@ const tempProduct = fs.readFileSync(
 );
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 ///////////////////////////
 // SERVER
 const server = http.createServer((req, res) => {
