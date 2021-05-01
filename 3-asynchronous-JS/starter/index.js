9fd15e9c37800335951ab8c826eac17aac2db0ef
Elango <elango@Elangos-MacBook-Pro.local>
2021-05-01 20:37:41 +0530

    42-callback_hell_to_promises

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -6,7 +6,7 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
 
     console.log(`Breed: ${data}`);
 
-    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
+    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).then(res => {
         if (err) return console.log(err.message)
 
         fs.writeFile('dog-img.txt', res.body.message, err => {
@@ -14,5 +14,5 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
 
             console.log("Random dog image save to file")
         })
-    })
+    }).catch(err => console.log(err.message))
 })
