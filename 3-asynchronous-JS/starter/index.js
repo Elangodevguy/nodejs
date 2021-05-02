815dbdf1e5fb9f396317b08a63307ee186ca9523
Elango <elango@Elangos-MacBook-Pro.local>
2021-05-02 16:25:17 +0530

    45-returning_values_from_async

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -31,10 +31,20 @@ const getDogPic = async () => {
         await writeFilePro(`dog-img.txt`, res.body.message)
         console.log('Random dog image saved to file!');
     } catch (err) {
-        console.log(err);
+        throw err;
     }
+    return '2: READY 🐶';
 };
-getDogPic()
+(async () => {
+    try {
+        console.log('1: Will get dog pics!');
+        const x = await getDogPic();
+        console.log(x);
+        console.log('3: Done getting dog pics!');
+    } catch (err) {
+        console.log('ERROR 💥');
+    }
+})();
 // readFilePro(`${__dirname}/dog.txt`)
 //     .then(data => {
 //         console.log(`Breed: ${data}`);
