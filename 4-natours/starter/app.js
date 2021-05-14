9aa9afb62100981fa993f3333e1e8a470755dc95
Elango <elango@Elangos-MacBook-Pro.local>
2021-05-14 14:07:55 +0530

    52-Starting_our_api

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,9 +1,20 @@
+const fs = require('fs');
 const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.status(200).send({ message: 'hi' })
+// app.get('/', (req, res) => {
+//     res.status(200).json({ message: 'hi' })
+// })
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        result: tours.length,
+        data: {
+            tours
+        }
+    })
 })
 
 const port = 8000
